aa138c406e16106060daab241d13f93d5a7efb90
Manivannan S <sharvamanivannan@gmail.com>
2023-03-27 10:30:59 +0000

    login page and routings

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
 import Login from './Pages/Login';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Register from './Pages/Register';
+import ForgotPassword from './Pages/ForgotPassword';
 
 function App() {
   return (
-    <div className="App">
-      <Login />
-    </div>
+    <Router>
+      <div className="App">
+        <Login />
+        <Routes>
+          <Route path='/register' element={<Register />} />
+          <Route path='/Forgotpassword' element={<ForgotPassword />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- myapp/src/Pages/Login.js
+++ myapp/src/Pages/Login.js
@@ -1,52 +1,52 @@
-import Logo from '../Assets/Images/Logo.webp';
 import '../Assets/Styles/Login.css'
+import { Link } from 'react-router-dom'
 
 const Login = () => {
     return (
-        <div class="limiter">
-            <div class="container-login100">
-                <div class="wrap-login100">
-                    <div class="login100-pic js-tilt" data-tilt="" style={{"will-change": "transform", "transform": "perspective(300px) rotateX(0deg) rotateY(0deg)"}}>
-                        <img src={Logo} alt="IMG"/>
+        <div className='page-bg'>
+            <div className='container'>
+                <div class="row justify-content-center">
+                    <div class="col-xl-10 col-lg-12 col-md-9">
+                        <div class="card o-hidden border-0 shadow-lg my-5">
+                            <div class="card-body p-0">
+                                <div class="row">
+                                    {/* Left-side-card */}
+                                    <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+                                    {/* Right-side-card */}
+                                    <div class="col-lg-6">
+                                        <div class="p-5">
+                                            <div class="text-center">
+                                                <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
+                                            </div>
+                                            <form>
+                                                <div class="mb-3">
+                                                    <label for="exampleInputEmail1" class="form-label">Email address</label>
+                                                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                                                    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                                                </div>
+                                                <div class="mb-3">
+                                                    <label for="exampleInputPassword1" class="form-label">Password</label>
+                                                    <input type="password" class="form-control" id="exampleInputPassword1" />
+                                                </div>
+                                                <div class="mb-3 form-check">
+                                                    <input type="checkbox" class="form-check-input" id="exampleCheck1" />
+                                                    <label class="form-check-label" for="exampleCheck1">Remember Me</label>
+                                                </div>
+                                                <button type="submit" class="btn btn-primary d-flex align-items-center justify-contents-center">Login</button>
+                                            </form>
+                                            <hr />
+                                            {/* <div class="text-center">
+                                                <Link class="small" to="/forgot-password">Forgot Password?</Link>
+                                            </div>
+                                            <div class="text-center">
+                                                <Link class="small" to="/register">Create an Account!</Link>
+                                            </div> */}
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <form class="login100-form validate-form">
-                        <span class="login100-form-title">
-                            Member Login
-                        </span>
-                        <div class="wrap-input100 validate-input" data-validate="Valid email is required: ex@abc.xyz">
-                            <input class="input100" type="text" name="email" placeholder="Email"/>
-                                <span class="focus-input100"></span>
-                                <span class="symbol-input100">
-                                    <i class="fa fa-envelope" aria-hidden="true"></i>
-                                </span>
-                        </div>
-                        <div class="wrap-input100 validate-input" data-validate="Password is required">
-                            <input class="input100" type="password" name="pass" placeholder="Password"/>
-                                <span class="focus-input100"></span>
-                                <span class="symbol-input100">
-                                    <i class="fa fa-lock" aria-hidden="true"></i>
-                                </span>
-                        </div>
-                        <div class="container-login100-form-btn">
-                            <button class="login100-form-btn">
-                                Login
-                            </button>
-                        </div>
-                        <div class="text-center p-t-12">
-                            <span class="txt1">
-                                Forgot
-                            </span>
-                            <a class="txt2" href="#!">
-                                Username / Password?
-                            </a>
-                        </div>
-                        <div class="text-center p-t-136">
-                            <a class="txt2" href="#!">
-                                Create your Account
-                                <i class="fa fa-long-arrow-right m-l-5" aria-hidden="true"></i>
-                            </a>
-                        </div>
-                    </form>
                 </div>
             </div>
         </div>
